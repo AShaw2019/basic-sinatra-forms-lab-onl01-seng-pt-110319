cc8556dde0dde8a1fba7347bdb1af42f993f24e1
Ashley Twomey <aprier091012@gmail.com>
2020-03-01 15:28:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,13 @@ get '/newteam' do
   end
 
   post '/team' do
-   puts params
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @center = params["c"]
    
     erb :team
   end
